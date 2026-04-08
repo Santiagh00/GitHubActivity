6a9e06b72005c4032a87d413f2edb0ec830dab04
CSSM01 <cristiansantiagosilvamunoz@gmail.com>
2026-04-08 07:42:32 -0500

    Agregado mensaje de inicio por Silva

--- practicaCrud/src/main/java/edu/co/ustavillavicencio/practicacrud/PracticaCrudApplication.java
+++ practicaCrud/src/main/java/edu/co/ustavillavicencio/practicacrud/PracticaCrudApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PracticaCrudApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PracticaCrudApplication.class, args);
-    }
+    public static void main(String[] args)   {
+;       SpringApplication.run(PracticaCrudApplication.class, args);
 
+        System.out.println("Proyecto incluido, Silva estuvo aqui");
+    }
 }
